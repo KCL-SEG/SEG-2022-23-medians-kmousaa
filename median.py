dcd3b021fd0c6ceda3b42bfb78178d70d1681a3a
Karim Mousa <karimmousa202@gmail.comgit config --global --replace-all user.email karimmousa202@gmail.com>
2022-09-28 23:28:28 +0100

    c

--- median.py
+++ median.py
@@ -14,11 +14,12 @@ numbers.sort()
 
 # [1,2,3,4,5,6,7]
 
+if n % 2 == 0:
+    median1 = n_num[n//2]
+    median2 = n_num[n//2 - 1]
+    median = (median1 + median2)/2
 
-if len(numbers) % 2 != 0: #odd 
-    print(numbers[(len(numbers)/2)])
-else: #even
-    floor = numbers[len(numbers)//2]
-    roof =  numbers[-(-len(numbers)//2)]
+else:
+    median = n_num[n//2]
 
-    print((floor+roof)/2)
+print(median)
